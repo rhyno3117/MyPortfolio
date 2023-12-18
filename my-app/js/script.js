37bd29b5496b031a7c8cf00c7f03ae00c194a8a6
ryan <rhynogolfer42@gmail.com>
2023-12-17 18:12:20 -0800

    Adds myfile.md

--- my-app/js/script.js
+++ my-app/js/script.js
@@ -1,10 +1,16 @@
+//toggle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
 
-
-
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
 
 //scroll sections
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
+
 
 window.onscroll = () => {
     sections.forEach(sec => {
@@ -17,7 +23,7 @@ window.onscroll = () => {
             //active navbar links
             navLinks.forEach(links => {
                 links.classList.remove('active');
-                document.querySelector('header nav a[href *=' + id + ']').classList.add('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
         }
     });
@@ -25,4 +31,8 @@ window.onscroll = () => {
     let header = document.querySelector('header');
 
     header.classList.toggle('sticky', window.scrollY > 100);
+
+    //remove toggle icon and navbar when click navbar links (scroll)
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 }
