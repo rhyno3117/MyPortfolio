bed9474a8576e48b7effa0de8d5b6d5033abf1e3
ryan <rhynogolfer42@gmail.com>
2023-12-22 15:00:48 -0800

    Adds myfile.md

--- my-app/js/script.js
+++ my-app/js/script.js
@@ -48,3 +48,36 @@ window.onscroll = () => {
 
     footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
 }
+
+
+
+// Contact form
+const form = document.querySelector('form');
+const fullName = document.getElementById('name');
+const email = document.getElementById('email');
+const phone = document.getElementById('phone');
+const subject = document.getElementById('subject');
+const mess = document.getElementById('message');
+
+
+function sendEmail(){
+const bodyMessage = `Full Name: ${fullName.value}<br> Email: ${email.value}<br> Phone: ${phone.value}<br> Message: ${mess.value}<br>`;
+
+
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "ryan.m.meyers42@gmail.com",
+        Password : "53FA171E819DDD296F1F0DFD8FBEE6564F5E",
+        To : 'ryan.m.meyers42@gmail.com',
+        From : "ryan.m.meyers42@gmail.com",
+        Subject : subject.value,
+        Body : bodyMessage
+    }).then(
+      message => alert(message)
+    );
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    sendEmail();
+});
