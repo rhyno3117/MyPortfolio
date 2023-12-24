6bb0fadeae0df85a9a15d7546c966cb45e6fe9fa
ryan <rhynogolfer42@gmail.com>
2023-12-23 16:55:58 -0800

    Adds myfile.md

--- my-app/js/script.js
+++ my-app/js/script.js
@@ -29,10 +29,10 @@ window.onscroll = () => {
             sec.classList.add('show-animate');
         }
         // if want to use animation that repeats on scroll use this
-    else{
-        sec.classList.remove('show-animate');
+        else {
+            sec.classList.remove('show-animate');
 
-    }
+        }
     });
     //sticky header
     let header = document.querySelector('header');
@@ -60,24 +60,88 @@ const subject = document.getElementById('subject');
 const mess = document.getElementById('message');
 
 
-function sendEmail(){
-const bodyMessage = `Full Name: ${fullName.value}<br> Email: ${email.value}<br> Phone: ${phone.value}<br> Message: ${mess.value}<br>`;
+function sendEmail() {
+    const bodyMessage = `Full Name: ${fullName.value}<br> Email: ${email.value}<br> Phone: ${phone.value}<br> Message: ${mess.value}<br>`;
 
 
     Email.send({
-        Host : "smtp.elasticemail.com",
-        Username : "ryan.m.meyers42@gmail.com",
-        Password : "53FA171E819DDD296F1F0DFD8FBEE6564F5E",
-        To : 'ryan.m.meyers42@gmail.com',
-        From : "ryan.m.meyers42@gmail.com",
-        Subject : subject.value,
-        Body : bodyMessage
+        SecureToken: "fd61e172-76b8-42ef-9ead-4e43a3d0fac5",
+        To: 'ryan.m.meyers42@gmail.com',
+        From: "ryan.m.meyers42@gmail.com",
+        Subject: subject.value,
+        Body: bodyMessage
     }).then(
-      message => alert(message)
+        message => {
+            if (message == 'OK') {
+                Swal.fire({
+                    title: "Success!",
+                    text: "Message sent successfully!",
+                    icon: "success"
+                });
+            }
+        }
+
     );
+}
+function checkInputs() {
+    const items = document.querySelectorAll('.item');
+
+    for (const item of items) {
+        if (item.value == "") {
+            item.classList.add('error');
+            item.parentElement.classList.add('error');
+        }
+
+        if (items[1].value != '') {
+            checkEmail();
+        }
+        items[1].addEventListener('keyup', () => {
+            checkEmail();
+        });
+
+        item.addEventListener('keyup', () => {
+            if (item.value != '') {
+                item.classList.remove('error');
+                item.parentElement.classList.remove('error');
+            }
+            else {
+                item.classList.add('error');
+                item.parentElement.classList.add('error');
+            }
+        });
+    }
+}
+
+function checkEmail() {
+    const emailRegex = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,3})(\.([a-z]{2,3}))?$/;
+    const errorTxtEmail = document.querySelector('.error-txt email');
+
+    if (!email.value.match(emailRegex)) {
+        email.classList.add('error');
+        email.parentElement.classList.add('error');
+
+        if (email.value != '') {
+            errorTxtEmail.innerText = 'Enter a valid email address';
+        }
+        else {
+            errorTxtEmail.innerText = "Email Address can't be blanks";
+
+        }
+    }
+    else {
+        email.classList.remove('error');
+        email.parentElement.classList.remove('error');
+    }
 }
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    sendEmail();
+    checkInputs();
+
+    if (!fullName.classList.contains('error') && !email.classList.contains('error') && !phone.classList.contains('error') && !subject.classList.contains('error') && !mess.classList.contains('error')) {
+        sendEmail();
+
+        form.reset();
+        return false;
+    }
 });
